265a419651d78269a83377ca7fb7aa20118e6150
mcaupybugs <vishalydv.me@gmail.com>
2024-08-05 17:00:43 +0530

    Adding the active tab in login

--- app/components/authentication/modal.tsx
+++ app/components/authentication/modal.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { AuthType } from '../navbar';
 import SignIn from './signIn';
 import SignUp from './signUp';
 
 const Modal = ({toggleAuth}) => {
+
+    const signInButton = useRef(null);
+    const signUpButton = useRef(null);
+
+    const makeActive = (authSelected : AuthType) => {
+        const inactiveClasses = "border-2 border-solid w-full ml-2 mr-2 text-slate-200 font-bold text-xl bg-slate-400"
+        const activeClasses = "border-2 border-solid w-full ml-2 mr-2 text-slate-700 font-bold text-xl border-t-4 border-slate-800"
+        var signInElement = signInButton.current;
+        var signUpElement = signUpButton.current;
+        if (!signInElement || !signUpElement){
+            return;
+        }
+        if(authSelected === AuthType.Login){
+            signInElement.className = activeClasses;
+            signUpElement.className = inactiveClasses;
+        }else{
+            signInElement.className = inactiveClasses;
+            signUpElement.className = activeClasses;
+        }
+    }
+
+    useEffect(()=>{
+        makeActive(AuthType.Login);
+    }, [])
+
     return(
         <div className='flex bg-white/30 h-screen w-screen justify-center items-center text-black'>
             <div className='flex flex-col h-4/5 w-2/5 bg-white rounded-md'>
@@ -14,8 +39,8 @@ const Modal = ({toggleAuth}) => {
                     </div>                
                 </div>
                 <div className='flex flex-row justify-evenly w-full h-16 mt-2'>
-                    <button className='border-2 border-solid w-full ml-4 text-slate-700 font-bold text-xl'>Sign In</button>
-                    <button className='border-2 border-solid w-full mr-4 text-slate-700 font-bold text-xl'>Sign Up</button>
+                    <button className='border-2 border-solid w-full ml-4 text-slate-700 font-bold text-xl' ref={signInButton} onClick={() => makeActive(AuthType.Login)}>Sign In</button>
+                    <button className='border-2 border-solid w-full mr-4 text-slate-700 font-bold text-xl' ref={signUpButton} onClick={() => makeActive(AuthType.SignUp)}>Sign Up</button>
                 </div>
             </div>
         </div>
