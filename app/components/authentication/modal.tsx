803f01aae4670281e12253f508b28ee754f4da25
mcaupybugs <vishalydv.me@gmail.com>
2024-08-05 15:14:50 +0530

    Adding modal updates

--- app/components/authentication/modal.tsx
+++ app/components/authentication/modal.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { AuthType } from '../navbar';
+import SignIn from './signIn';
+import SignUp from './signUp';
 
 const Modal = ({toggleAuth}) => {
     return(
         <div className='flex bg-white/30 h-screen w-screen justify-center items-center text-black'>
-            <div className='flex flex-col h-4/5 w-3/5 bg-white rounded-md'>
-                <div className='flex flex-row justify-center items-center'>
+            <div className='flex flex-col h-4/5 w-2/5 bg-white rounded-md'>
+                <div className='flex flex-row justify-center items-center bg-slate-300'>
                     <span className='w-fit text-3xl items-start p-2 hover:cursor-pointer' onClick={() => toggleAuth(AuthType.Close)}> &#x2716; </span>
-                    <p className='w-full h-full text-center flex-grow items-center'>Please login to continue</p>
+                    <div className='flex-grow flex justify-center items-center h-full'>
+                        <p className='text-center'>Please login to continue</p>
+                    </div>                
+                </div>
+                <div className='flex flex-row justify-evenly w-full h-16 mt-2'>
+                    <button className='border-2 border-solid w-full ml-4 text-slate-700 font-bold text-xl'>Sign In</button>
+                    <button className='border-2 border-solid w-full mr-4 text-slate-700 font-bold text-xl'>Sign Up</button>
                 </div>
             </div>
         </div>
---
+++ app/components/authentication/signIn.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignIn = () => {
+    return (
+        <div>
+            Hey sign in.
+        </div>
+    )
+}
+
+export default SignIn;
