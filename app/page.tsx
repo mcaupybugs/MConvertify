134d4b5a4c6958654083f052ae86715d0175d18b
mcaupybugs <vishalydv.me@gmail.com>
2024-08-15 20:23:24 +0530

    add no overflow in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+"use client"
+import { useEffect } from "react";
+
 export default function Home() {
+
+  useEffect(()=>{
+    document.body.classList.add('overflow-y-hidden')
+  })
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
