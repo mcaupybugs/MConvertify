6d90c8d3f9114ba19b0a28c883386559728f1243
mcaupybugs <vishalydv.me@gmail.com>
2024-08-16 17:30:03 +0530

    added operate button to be changed later

--- app/operations/textcomparer/page.tsx
+++ app/operations/textcomparer/page.tsx
@@ -3,9 +3,14 @@ import InputBox from '../../components/inputBox';
 
 const TextComparer = () => {
     return (
-        <div className='flex flex-row w-full h-full justify-evenly'>
-            <InputBox heading='Input'></InputBox>
-            <InputBox heading='Output'></InputBox>
+        <div className='flex flex-col w-full h-full'>
+            <div className='flex w-full justify-center pt-2 h-20'>
+                <button className='h-14 w-36 rounded-full border-white border-2'>Operate</button>
+            </div>
+            <div className='flex flex-1 flex-row w-full h-full justify-evenly'>
+                <InputBox heading='Input'></InputBox>
+                <InputBox heading='Output'></InputBox>
+            </div>
         </div>
     )
 }
