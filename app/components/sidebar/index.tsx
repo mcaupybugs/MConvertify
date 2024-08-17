847c08b5bd12acf0a790b4db546640199e4b76f3
mcaupybugs <vishalydv.me@gmail.com>
2024-08-17 16:17:05 +0530

    Adding sidebar UI improvements

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -4,11 +4,12 @@ import React from "react";
 const SideBar = () =>{
     return (
         <div className="w-full h-full basis-2/12 pt-2 pb-2">
-        <div className="w-full h-full flex flex-col rounded-md border-2 border-white text-center p-2">
-            <div className="w-full font-bold text-center underline">
+        <div className="w-full h-full flex flex-col rounded-md border-2 border-slate-300 text-center gap-2">
+            <div className="w-full font-bold text-center text-lg bg-slate-300 rounded-sm">
                 Operations
             </div>
-            <Link href='/operations/textcomparer'>Text Comparer</Link>
+            <Link href='/operations/textcomparer' className="w-full text-center border-b-2 border-black">Text Comparer</Link>
+            <Link href='/operations/textbeautifier' className="w-full text-center border-b-2 border-black">Text Beautifier</Link>
         </div>
         </div>
     )
