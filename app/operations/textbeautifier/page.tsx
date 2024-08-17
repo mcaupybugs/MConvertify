83c85c98fb871f328a2a22ec05fdf5f7bb8270b9
mcaupybugs <vishalydv.me@gmail.com>
2024-08-17 16:55:14 +0530

    Adding text beautifier code

---
+++ app/operations/textbeautifier/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+import InputBox from "@/app/components/inputBox";
+import OutputBox from "@/app/components/outputBox";
+import React, { useState } from "react";
+
+const TextBeautifier = () => {
+    const [inputText, setInputText] = useState('');
+    const [outputData, setOutputData] = useState('');
+    const getInputText = (text) => {
+        setInputText(text);
+    }
+
+    const handleOperate = (e) => {
+
+    }
+
+    return (
+        <div className='flex flex-col w-full h-full'>
+            <div className='flex w-full justify-center pt-2 h-20'>
+                <button className='h-14 w-36 rounded-full border-black border-2' onClick={(e) => handleOperate(e)}>Operate</button>
+            </div>
+            <div className='flex flex-1 flex-row w-full h-full justify-evenly'>
+                    <InputBox heading='Input' textCallback={getInputText}></InputBox>
+                    <OutputBox heading='Output' outputValue={outputData}></OutputBox>
+            </div>
+        </div>
+    )
+}
+
+export default TextBeautifier;
