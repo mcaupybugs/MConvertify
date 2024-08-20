4c84b41ff7d679ed7f852839fe0d8def8fa4be2e
Vishal Yadav <vishalydv.me@gmail.com>
2024-08-20 05:43:51 +0530

    Making a bit change in the header adding language buttons

--- app/components/inputBox/index.tsx
+++ app/components/inputBox/index.tsx
@@ -21,7 +21,25 @@ const InputBox = ({ heading, textCallback = null }) => {
                     <div className='flex w-full h-full text-black text-xl font-bold pl-2 items-center'>
                         {heading}
                     </div>
-                    <div></div>
+                    <div className='h-full w-full flex flex-row gap-4'>
+                        <div className='w-full h-full'> {/* add the language */}
+                            <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option selected value="javascript">JS</option>
+                                <option value="CA">Canada</option>
+                                <option value="FR">France</option>
+                                <option value="DE">Germany</option>
+                            </select>
+                        </div>
+                        <div className='w-full h-full'>
+                            <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option selected>Choose a country</option>
+                                <option value="US">United States</option>
+                                <option value="CA">Canada</option>
+                                <option value="FR">France</option>
+                                <option value="DE">Germany</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
                 <div className='flex-1 overflow-auto'>
                     <Editor></Editor>
@@ -45,7 +63,7 @@ const InputBox = ({ heading, textCallback = null }) => {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
